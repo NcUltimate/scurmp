6bbd98a0913ebcf4799741491af7eedf8746aa2d
Joseph Klemen <joe.jklemen@qualia.com>
2022-10-16 09:16:06 -0400

    Better handling around missing tracks

--- runner.js
+++ runner.js
@@ -1,8 +1,71 @@
 RUNNER = {
   BOOKMARK: 100,
-  NO_MATCH: [],
+  MISSING: {},
 
-  waitForOverlayToClose() {
+  async init() {
+    if(!SC.$) {
+      console.log('Initializing SC...');
+      await SC.init();
+      console.log('SC Initialized.');
+    }
+
+    if(this.MISSING.length === 0 && NO_MATCH) {
+      this.MISSING = NO_MATCH;
+    }
+  },
+
+  async run(start = this.BOOKMARK, end = LIBRARY.length) {
+    const shouldUpdateBookmark = (start === this.BOOKMARK);
+    
+    for(var trackNumber = start; trackNumber < end; trackNumber++) {
+      this.process(trackNumber);
+
+      if(shouldUpdateBookmark) {
+        this.BOOKMARK = trackNumber;
+      }
+    }
+  },
+
+  async runSpecific(indices) {
+    for(const trackNumber of indices) {
+      await this.process(trackNumber);
+    }
+  },
+
+  async process(trackNumber) {
+    if(trackNumber > LIBRARY.length) {
+      return false;
+    }
+
+    this.init();
+
+    const spotifyTrack = LIBRARY[trackNumber];
+
+    const remixParam =
+        spotifyTrack.is_remix ? { remixArtist: spotifyTrack.remix_artist } : {};
+      
+    const result = await SC.identify(
+      spotifyTrack.name,
+      spotifyTrack.artists,
+      remixParam,
+    );
+
+    const playlistAddResult = await SC.addToPlaylist(result);
+    if(playlistAddResult.reason === 'no_match') {
+      this.MISSING[trackNumber] = spotifyTrack;
+    }
+
+    console.log([
+      trackNumber.toString().padStart(4, '0'),
+      spotifyTrack.name.slice(0, 20).padEnd(20, ' '),
+      spotifyTrack.artists.join(', ').slice(0, 30).padEnd(30, ' '),
+      playlistAddResult.reason.padEnd(15, ' '),
+    ].join(' | '));
+
+    await this._waitForOverlayToClose();
+  },
+
+  _waitForOverlayToClose() {
     return new Promise((resolve) => {
       const waitForOverlayToCloseID = setInterval(() => {
         const $playlistModal = SC.$('[id*=overlay].modal .modal__modal');
@@ -12,41 +75,5 @@ RUNNER = {
         }
       }, 100);
     });
-  },
-
-  async run(start = this.BOOKMARK, end = LIBRARY.length) {
-    if(!SC.$) {
-      console.log('Initializing SC...');
-      await SC.init();
-      console.log('SC Initialized.');
-    }
-
-    let currentIndex = start;
-    for(const spotifyTrack of LIBRARY.slice(start, end)) {
-      const remixParam =
-        spotifyTrack.is_remix ? { remixArtist: spotifyTrack.remix_artist } : {};
-      
-      const result = await SC.identify(
-        spotifyTrack.name,
-        spotifyTrack.artists,
-        remixParam,
-      );
-
-      result.index = currentIndex++;
-
-      const playlistAddResult = await SC.addToPlaylist(result);
-      if(playlistAddResult.reason === 'no_match') {
-        NO_MATCH.push(result);
-      }
-
-      console.log([
-        result.index.toString().padStart(4, '0'),
-        spotifyTrack.name.slice(0, 20).padEnd(20, ' '),
-        spotifyTrack.artists.join(', ').slice(0, 30).padEnd(30, ' '),
-        playlistAddResult.reason.padEnd(15, ' '),
-      ].join(' | '));
-
-      await this.waitForOverlayToClose();
-    }
   },
 };
